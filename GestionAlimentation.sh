1606d9831fcf745792558351e4085e7b79d67151
Kira JA <marilynwildschool@gmail.com>
2024-11-01 16:34:56 +0100

    modification script gestionAlimentation.sh

--- GestionAlimentation.sh
+++ GestionAlimentation.sh
@@ -10,17 +10,14 @@ fonction_arret() {
 
     case $confirmationArret in
         1)
-            # Arrêt immédiat
             echo "Arrêt immédiat en cours..."
             sudo shutdown now
             ;;
         2)
-            # Arrêt avec délai d'une minute
             echo "Arrêt dans une minute en cours..."
             sudo shutdown +1
             ;;
         3)
-            # Annuler l'arrêt
             echo "Arrêt annulé."
             return
             ;;
@@ -40,17 +37,14 @@ fonction_redemarrage() {
 
     case $confirmationRedemarrage in
         1)
-            # Redémarrage immédiat
             echo "Redémarrage immédiat en cours..."
             sudo reboot now
             ;;
         2)
-            # Redémarrage avec délai d'une minute
             echo "Redémarrage dans une minute en cours..."
             sudo shutdown -r +1
             ;;
         3)
-            # Annuler le redémarrage
             echo "Redémarrage annulé."
             return
             ;;
@@ -63,16 +57,16 @@ fonction_redemarrage() {
 # Fonction pour verrouiller la session
 fonction_verrouillage() {
     echo "Verrouillage de la session..."
-    # Cette commande verrouille l'écran pour Ubuntu/Gnome. Modifiez-la selon votre gestionnaire de fenêtres si nécessaire.
     gnome-screensaver-command -l || echo "Verrouillage non disponible."
 }
 
-# Fonction principale de gestion de l'alimentation
-gestion_alimentation() {
+# Menu principal en boucle
+while true; do
     echo "Que voulez-vous faire ?"
     echo "1 - Arrêter l'ordinateur"
     echo "2 - Redémarrer l'ordinateur"
     echo "3 - Verrouiller la session"
+    echo "4 - Quitter"
     read -p "Choix : " choixUtilisateur
 
     case $choixUtilisateur in
@@ -85,9 +79,12 @@ gestion_alimentation() {
         3)
             fonction_verrouillage
             ;;
+        4)
+            echo "Sortie du programme."
+            break
+            ;;
         *)
             echo "Choix invalide, veuillez réessayer."
-            gestion_alimentation  # Relance la fonction en cas de choix invalide
             ;;
     esac
-}
+done
