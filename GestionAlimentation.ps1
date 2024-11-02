1d8103ff89450e2e891d2416fbef25fed197c79b
Kira JA <marilynwildschool@gmail.com>
2024-11-02 10:05:57 +0100

    création script gestionAlimentation.ps1

---
+++ GestionAlimentation.ps1
@@ -0,0 +1,82 @@
+# Fonction pour arrêter l'ordinateur
+function Fonction-Arret {
+    Write-Output "Voulez-vous vraiment arrêter ?"
+    Write-Output "1 - Arrêt immédiat"
+    Write-Output "2 - Arrêt dans une minute"
+    Write-Output "3 - Annuler"
+    $confirmationArret = Read-Host "Choix"
+
+    switch ($confirmationArret) {
+        1 {
+            Write-Output "Arrêt immédiat en cours..."
+            Stop-Computer -Force
+        }
+        2 {
+            Write-Output "Arrêt dans une minute en cours..."
+            shutdown.exe /s /t 60
+        }
+        3 {
+            Write-Output "Arrêt annulé."
+            return
+        }
+        default {
+            Write-Output "Choix invalide."
+        }
+    }
+}
+
+# Fonction pour redémarrer l'ordinateur
+function Fonction-Redemarrage {
+    Write-Output "Voulez-vous vraiment redémarrer ?"
+    Write-Output "1 - Redémarrage immédiat"
+    Write-Output "2 - Redémarrage dans une minute"
+    Write-Output "3 - Annuler"
+    $confirmationRedemarrage = Read-Host "Choix"
+
+    switch ($confirmationRedemarrage) {
+        1 {
+            Write-Output "Redémarrage immédiat en cours..."
+            Restart-Computer -Force
+        }
+        2 {
+            Write-Output "Redémarrage dans une minute en cours..."
+            shutdown.exe /r /t 60
+        }
+        3 {
+            Write-Output "Redémarrage annulé."
+            return
+        }
+        default {
+            Write-Output "Choix invalide."
+        }
+    }
+}
+
+# Fonction pour verrouiller la session
+function Fonction-Verrouillage {
+    Write-Output "Verrouillage de la session..."
+    rundll32.exe user32.dll,LockWorkStation
+}
+
+# Menu principal en boucle
+while ($true) {
+    Write-Output "Que voulez-vous faire ?"
+    Write-Output "1 - Arrêter l'ordinateur"
+    Write-Output "2 - Redémarrer l'ordinateur"
+    Write-Output "3 - Verrouiller la session"
+    Write-Output "4 - Quitter"
+    $choixUtilisateur = Read-Host "Choix"
+
+    switch ($choixUtilisateur) {
+        1 { Fonction-Arret }
+        2 { Fonction-Redemarrage }
+        3 { Fonction-Verrouillage }
+        4 {
+            Write-Output "Sortie du programme."
+            break
+        }
+        default {
+            Write-Output "Choix invalide, veuillez réessayer."
+        }
+    }
+}
