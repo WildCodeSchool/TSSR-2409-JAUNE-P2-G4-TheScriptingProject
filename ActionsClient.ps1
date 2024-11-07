8e157bd4e6d0b813bd9b06df858cf36ec47da87e
pa-jovet <pa.jovet1@gmail.com>
2024-11-07 22:39:08 +0100

    Ajout du Module PowerShell contenant les fonctions des Actions Client 8 à 14

---
+++ ActionsClient.ps1
@@ -0,0 +1,241 @@
+# Arrêt
+function Arret {
+    Write-Output "Voulez-vous vraiment arrêter ?"
+    Write-Output "1 - Arrêt immédiat"
+    Write-Output "2 - Arrêt dans une minute"
+    Write-Output "3 - Annuler"
+    $confirmationArret = Read-Host "Choix"
+
+    switch ($confirmationArret) {
+        1 {
+            Write-Output "Arrêt immédiat en cours..."
+            Stop-Computer -Force
+        }
+        2 {
+            Write-Output "Arrêt dans une minute en cours..."
+            shutdown.exe /s /t 60
+        }
+        3 {
+            Write-Output "Arrêt annulé."
+            return
+        }
+        default {
+            Write-Output "Choix invalide."
+        }
+    }
+}
+
+# Redémarrage
+function Redemarrage {
+    Write-Output "Voulez-vous vraiment redémarrer ?"
+    Write-Output "1 - Redémarrage immédiat"
+    Write-Output "2 - Redémarrage dans une minute"
+    Write-Output "3 - Annuler"
+    $confirmationRedemarrage = Read-Host "Choix"
+
+    switch ($confirmationRedemarrage) {
+        1 {
+            Write-Output "Redémarrage immédiat en cours..."
+            Restart-Computer -Force
+        }
+        2 {
+            Write-Output "Redémarrage dans une minute en cours..."
+            shutdown.exe /r /t 60
+        }
+        3 {
+            Write-Output "Redémarrage annulé."
+            return
+        }
+        default {
+            Write-Output "Choix invalide."
+        }
+    }
+}
+
+# Verrouillage
+function Verrouillage {
+    Write-Output "Verrouillage de la session..."
+    rundll32.exe user32.dll,LockWorkStation
+}
+
+# Mise-à-jour du système
+function MiseAJour() {
+    #Vérification des mises à jour
+    write-host "Veuilez patienter pendant la recherche des mises à jours disponibles."
+
+    Get-WindowsUpdate
+    if ( $? -lt 0 )
+    {
+    $ValidationMAJ = Read-Host "Souhaitez-vous installer les mises à jours (o/n)"
+        if ( $ValidationMAJ -eq "o")
+        { 
+        write-host "La mise à jour du système peut prendre plusieurs minutes et necessiter un redémarrage de l'ordinateur."
+        Install-WindowsUpdate -AcceptAll
+        Write-Host "La mise à jour du système d'exploitation est effèctuée" -ForegroundColor green
+        }
+        else 
+        {
+        Write-Host "Abandon de l'opération de mise à jour du système d'exploitation" -ForegroundColor Red
+        menuDeRetour
+        }
+    }
+    else
+    {
+    Write-Host "Aucune mise à jours nécessaire" -ForegroundColor green
+    menuDeRetour
+    }
+}
+
+# Création de répertoire
+function CréaRep {
+    Clear-Host
+    $Chemin = Read-Host "Où souhaitez-vous créer un répertoire (nom avec chemin absolu)" 
+    if ( Test-Path -Path $Chemin -PathType Container )
+    {
+        Write-Host "Le répertoire $chemin éxiste déjà!"
+    }
+    else
+    {
+        New-Item -Path $Chemin -ItemType Directory -ErrorAction Stop | Out-Null
+        Write-Host"Répertoire $Chemin créé!"    
+    }
+}
+
+# Modification de répertoire
+function ModifRep {
+    Clear-Host
+    Write-Host "choix 1: Modifier le nom d'un répertoire."
+    Write-Host "choix 2: Déplacer un répertoire."
+    Write-Host "choix 3: Retour au menu précédant."
+    Write-Host "choix 4: Retour au menu principal."
+    $SelectModiRep = Read-Host "Quelle opération souhaitez-vous réaliser?" 
+
+    switch ( $SelectModiRep )
+    {
+        1{  $NameRep = Read-Host "Quel est le nom actuel du répertoire" 
+            $PathRep = Read-Host "Quel est le chemin du répertoire (chemin absolu)"
+            #SI le répertoire existe
+            if ( Test-Path "$PathRep\$NameRep" ) 
+            {
+                $NewNameRep = Read-Host "Quel sera le nouveau nom du répertoire"
+                #Vérification que le nouveau nom n'est pas déjà utilisé
+                if ( Test-Path "$PathRep\$NewNameRep" ) 
+                {
+                    Write-Host "Le nom de répertoire est déjà utilisé"
+                    ModifRep
+                }
+                else
+                {
+                    #Demande de confirmation de renommage du répertoire
+                    $ValRename = Read-Host "Vous souhaitez renommer le répertoire $NameRep en $NewNameRep (o/n)"
+                    if ( "$ValRename" -eq "o" ) 
+                    {
+                        Rename-Item -Path "$PathRep\$NameRep" -NewName "$NewNameRep"
+                        Write-Host "Le répertoire $NameRep a bien été renommer $NewNameRep"
+                        MenuGestionRep
+                    }
+                    else
+                    {
+                        Write-Host "L'opération de renommage du répertoire $NameRep a été abandonnée!"
+                        MenuGestionRep
+                    }
+                }
+            }    
+            else
+            {
+                write-host "Le répertoire indiqué n'a pas été trouvé"
+                MenuGestionRep
+            }
+        }
+
+        2{  $NameRepMove = Read-Host "Quel est le nom actuel du répertoire: " 
+            $PathRepMove = Read-Host "Quel est le chemin du répertoire (chemin absolu)"
+            #SI le répertoire existe
+            if ( Test-Path "$PathRepMove\$NameRepMove") 
+            {
+                $NewPathRep = Read-Host "Où souhaitez-vous déplacer le répertoire?"
+                #Vérification que le nouveau nom n'est pas déjà utilisé
+                if ( Test-Path "$PathRepMove\$NewPathRep" ) 
+                {
+                    Write-Host "Le nom et l'emplacement du répertoire existe déjà."
+                    ModifRep
+                }
+                else
+                {
+                    #Demande de confirmation de renommage du répertoire
+                    $ValMove = Read-Host "Vous souhaitez déplacer le répertoire $NameRepMove dans $NewPathRep (o/n)"
+                    if ( "$ValMove" -eq "o" ) 
+                    {
+                        Move-Item -Path "$PathRepMove\$NameRepMove" -Destination "$NewPathRep\$NameRepMove"
+                        Write-Host "Le répertoire $NameRepMove a bien été déplacé dans $WewPathRep"
+                        MenuGestionRep
+                    }
+                    else
+                    {
+                        Write-Host "Le déplacement du répertoire $NameRepMove a été annulé!"
+                        MenuGestionRep
+                    }
+                }
+            }    
+            else
+            {
+                write-host "Le répertoire indiqué n'a pas été trouvé"
+                MenuGestionRep
+            }
+        }
+        3{ MenuGestionRep } 
+        4{ Menu1 }
+    }
+}
+
+# Suppression de répertoire
+function SuppRep {
+    Clear-Host
+    $ChoixSup = Read-Host "Souhaitez vous effectuer la suppression d'un répertoire(o/n)" 
+    if ( "$ChoixSup" -eq "o" ) 
+    {
+            $PathRepSup = Read-Host "Quel est le repertoire à supprimer (avec son chemin absolu)" 
+        if ( test-Path "$PathRepSup" ) 
+        {
+            $ConfSup = Read-Host "Souhaitez-vous supprimer le répertoire $PathRepSup, cette action sera irrévocable(o/n)" 
+            if ( "$confSup" -eq "o" )
+            {
+                Remove-Item "$PathRepSup"
+                Write-Host "Répertoire $PathRepSup supprimé avec succès"
+                MenuGestionRep
+            }
+            else
+            {
+                Write-Host "Suppression annulée"
+                MenuGestionRep
+            }
+        }
+        else
+        {
+        echo "Le répertoire sélectionné n'a pas été trouvé"
+        SuppRep
+        }
+    }
+    else
+    {
+        menuGestionRepertoires
+    }
+}
+
+# Prise de main à distance (GUI)
+
+
+# Activation du pare-feu
+# Déjà dans le menu
+
+# Désactivation du pare-feu
+# Déjà dans le menu
+
+
+# Installation de logiciel
+
+
+# Désinstallation de logiciel
+
+
+# Exécution de script sur la machine distante
