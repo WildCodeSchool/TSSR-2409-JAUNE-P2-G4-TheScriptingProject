a2427c5e607c9d9d2d3c0ce83d53fe95621bf77b
Christophe <chris.rou21000@gmail.com>
2024-11-08 10:47:32 +0100

    Liste des ports

--- portsouverts.sh
+++ portsouverts.sh
@@ -3,21 +3,9 @@
 function portsOuverts() {
     echo "Les ports ouverts sur le système sont:"
     ss -tulnp | grep LISTEN | awk '{print $5}' | cut -d ':' -f2 | sort -u
+    echo "================================="
+    read -p "Appuyer sur entrer pour revenir au menu" 
 }
-
-function retour () {
-    echo "Retour au menu précédant choix 1"
-    echo "Retour au menu principal choix 2"
-    read  -p "Faite votre selection (1/2) :" selectRet
-        if [ "$selectRet" -eq 1 ]
-        then
-            echo return
-        else
-            echo menu1
-        fi     
-}
-
 
 portsOuverts
-echo "================================="
-retour
+
