057f3fea1f12317820fa29af7024de75dc7a5ea3
Marilyn Jacques-Sébastien <marilynwildschool@gmail.com>
2024-11-12 18:28:55 +0100

    modification script connexion.ps1

--- connexionGui.ps1
+++ connexionGui.ps1
@@ -1,3 +1,8 @@
+Write-Output "============================="
+Write-Output "Prise en main à distance Gui"
+Write-Output "============================="
+Write-Output ""
+
 # Récupérer l'adresse IP du client
 $remoteComputer = "adresse_IP_du_client"  # Remplacez par l'adresse IP du client
 
